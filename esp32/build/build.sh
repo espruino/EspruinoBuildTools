15b86eb11479bbe196fa63595d2c033586bc8f88
Rhys.Williams <github@wilberforce.co.nz>
2017-03-01 20:06:22 +1300

    initial build

---
+++ esp32/build/build.sh
@@ -0,0 +1,4 @@
+# this script will build the  app.tgz and esp-idf.tgz from the sources
+# for now  just copying pre-built
+cp ../../../espruino-esp32/app.tgz ../deploy
+cp ../../../espruino-esp32/esp-idf.tgz ../deploy
