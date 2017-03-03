9328c7525085e6ee8c30753277637976bb9919f3
wilberforce <github@wilberforce.co.nz>
2017-03-04 12:41:46 +1300

    Fix installer

--- esp32/build/build.sh
+++ esp32/build/build.sh
@@ -1,4 +1,6 @@
+if [ ! -d "Espruino" ]; then
 git clone https://github.com/espruino/Espruino.git
-source ./Espruino/scripts/provision-firmware.sh ESP32
+fi
+source ./Espruino/scripts/provision-framework.sh ESP32
 cd Espruino
-make
+make clean && make
