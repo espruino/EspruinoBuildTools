e02d926fce87614adb995bddf52df083c6075ec7
wilberforce <github@wilberforce.co.nz>
2017-04-07 08:38:41 +1200

    Update to esp-idf v2.0 (release)

--- esp32/build/build-idf.sh
+++ esp32/build/build-idf.sh
@@ -17,7 +17,7 @@ export IDF_PATH=$ESP_IDF_PATH
 git submodule update --init
 cd esp-idf
 git fetch
-git checkout v2.0-rc2
+git checkout v2.0
 git submodule update --init
 cd ..
 # adjust paths to this folder versions
