454ad91bf8ed070cb1fab0864b79cb2f46ac4f99
Rhys Williams <gitbub@wilberforce.co.nz>
2018-05-11 20:45:26 +1200

    new partition table - js_code now 0x2C0000

---
+++ esp32/build/build-tgz.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# adjust paths to this folder versions
+export ESP_IDF_PATH=`pwd`/esp-idf
+export ESP_IDF_PATH=`pwd`/esp-idf
+export ESP_APP_TEMPLATE_PATH=`pwd`
+
+cd app
+make app.tgz
+cd ../Espruino
+
+# copy newly build libs and expand
+tar xfz ../../deploy/esp-idf.tgz
+tar xfz ../../deploy/app.tgz
+
+return
+git fetch
+git checkout $espruino_branch
+git pull
+# reset the IDF_PATH
+source ./scripts/provision.sh ESP32
+make clean
+BOARD=ESP32 make
+ln -s app/build/bootloader/bootloader.bin
+ln -s app/build/partitions_espruino.bin
+cat targets/esp32/README_flash.txt
